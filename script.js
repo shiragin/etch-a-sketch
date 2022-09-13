dfe818930cfec16d096cf79287593bc7e26b9468
Shira Ginat <shiragin@gmail.com>
2022-09-14 01:59:27 +0300

    Finish createSquares function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+"use strict";
 
+// Variables
+const container = document.querySelector(".container");
+
+let squares = 32;
+
+// Creating divs inside the container box
+function createSquares(squares) {
+  for (let i = 0; i < squares * squares; i++) {
+    let div = document.createElement("div");
+    div.classList.add("square");
+    div.style.width = `${700 / squares}px`;
+    div.style.height = `${700 / squares}px`;
+    console.log(div.style.width, div.style.height);
+    container.append(div);
+    // console.log(container.childNodes);
+  }
+}
+
+createSquares(squares);
