38d453259e31d5ee0f3b8d830385160eeacca3bc
Shira Ginat <shiragin@gmail.com>
2022-09-14 03:28:43 +0300

    Finish choose size function

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 
 // Variables
 const container = document.querySelector(".container");
+const btnSize = document.querySelector(".btn-size");
 
-let squares = 16;
+// let squares = 16;
 
-// Creating divs inside the container box
+// Creating divs inside the container box & hover effect
 function createSquares(squares) {
   for (let i = 0; i < squares * squares; i++) {
     let div = document.createElement("div");
     div.classList.add("square");
     div.style.width = `${700 / squares}px`;
     div.style.height = `${700 / squares}px`;
-    console.log(div.style.width, div.style.height);
     container.append(div);
     div.addEventListener("mouseover", function () {
       div.style.backgroundColor = "#333";
@@ -20,6 +20,18 @@ function createSquares(squares) {
   }
 }
 
-createSquares(squares);
+function clearSquares() {
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
+}
 
-// Create hover effect by event listener
+createSquares(16);
+
+// Let user select box size
+btnSize.addEventListener("click", function (event) {
+  event.preventDefault();
+  let squares = document.querySelector("input[name=csize]:checked").value;
+  clearSquares();
+  createSquares(squares);
+});
